6fbdbecc54bac57a04933c1f189e4933329eb925
jesselangdon <jesselangdon@gmail.com>
2017-02-17 18:34:29 -0800

    Refactored Planform tool to work with revised Sinousity and Transfer Line Attributes tool.

--- ValleyPlanform.py
+++ ValleyPlanform.py
@@ -23,34 +23,31 @@ import gis_tools
 import Sinuosity
 import TransferAttributesToLine
 
-def main(fcStreamNetwork,fcValleyCenterline,fcValleyBottomPolygon,outputFCSinuosityChannel,outputFCSinuosityValley,outputFCPlanform,workspaceTemp="In_Memory"):
+def main(fcStreamNetwork,fcValleyCenterline,fcValleyBottomPolygon,outputFCSinuosityChannel,outputFCSinuosityValley,outputFCPlanform,workspaceTemp="in_memory"):
     
-    ## Set Workspace and Reset Modules
+    # Set Workspace and Reset Modules
     reload(TransferAttributesToLine)
     reload(Sinuosity)
 
-    ## Calculate Valley Sinuosity for each Channel Network segment
+    # Calculate valley sinuosity for each channel network segment
     if arcpy.Exists(outputFCSinuosityChannel):
         arcpy.Delete_management(outputFCSinuosityChannel)
-    arcpy.CopyFeatures_management(fcStreamNetwork,outputFCSinuosityChannel)
-    Sinuosity.main(outputFCSinuosityChannel,"Channel_Sinuosity",workspaceTemp)
+    Sinuosity.main(fcStreamNetwork, outputFCSinuosityChannel, "C_Sin", workspaceTemp)
     
-    ## Calculate Centerline Sinuosity for each Valley centerline segment.
+    # Calculate centerline sinuosity for each valley centerline segment.
     if arcpy.Exists(outputFCSinuosityValley):
         arcpy.Delete_management(outputFCSinuosityValley)
-    arcpy.CopyFeatures_management(fcValleyCenterline,outputFCSinuosityValley)
-    Sinuosity.main(outputFCSinuosityValley,"Valley_Sinuosity",workspaceTemp)
+    Sinuosity.main(fcValleyCenterline, outputFCSinuosityValley, "V_Sin", workspaceTemp)
 
-    ## Transfer Attributes to Stream Linework
+    # Transfer attributes to stream network polyline feature class
     if arcpy.Exists(outputFCPlanform):
         arcpy.Delete_management(outputFCPlanform)
     TransferAttributesToLine.main(outputFCSinuosityValley,
                                   outputFCSinuosityChannel,
-                                  "",
                                   outputFCPlanform,
                                   workspaceTemp)
 
-    ## Calculate Planform for each segment
+    # Calculate planform for each segment
     # Planform = channel sinuosity/valley sinuosity
     fieldPlanform = gis_tools.resetField(outputFCPlanform,"Planform","DOUBLE")
     codeblock = """def calculatePlanform(channel,valley):
@@ -61,10 +58,20 @@ def main(fcStreamNetwork,fcValleyCenterline,fcValleyBottomPolygon,outputFCSinuos
 
     arcpy.CalculateField_management(outputFCPlanform,
                                     fieldPlanform,
-                                    "calculatePlanform(!Channel_Sinuosity!,!Valley_Sinuosity!)",
+                                    "calculatePlanform(!C_Sin!,!V_Sin!)",
                                     "PYTHON",
                                     codeblock
                                     )
+
+    keepFields = [arcpy.Describe(outputFCPlanform).OIDFieldName,
+                  arcpy.Describe(outputFCPlanform).shapeFieldName,
+                  "Planform"]
+    dropFields = [dropField.name for dropField in arcpy.ListFields(outputFCPlanform)]
+    arcpy.MakeTableView_management(outputFCPlanform, "fcOutputView")
+    for dropField in dropFields:
+        if dropField not in keepFields:
+            arcpy.DeleteField_management("fcOutputView", dropField)
+
 
     return
 
