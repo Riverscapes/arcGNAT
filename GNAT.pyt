00ef89e5ee5c5548fffae4a237923373249ae11b
jesselangdon <jesselangdon@gmail.com>
2017-05-03 11:29:41 -0700

    Refactored Transfer Attribute tool to better handle differing spatial extents.

--- GNAT.pyt
+++ GNAT.pyt
@@ -11,7 +11,7 @@
 #                                                                             #
 # Created:     2015-Jan-08                                                    #
 # Version:     2.0 Beta                                                       #
-# Revised:     2017-Feb-17                                                    #
+# Revised:     2017-May-03                                                    #
 # Released:                                                                   #
 #                                                                             #
 # License:     MIT License                                                    #
@@ -35,7 +35,7 @@ import GenerateStreamBranches
 import Segmentation
 import FindNetworkFeatures
 
-GNAT_version = "2.1.01"
+GNAT_version = "2.1.02"
 
 strCatagoryStreamNetworkPreparation = "Main\\Step 1 - Stream Network Preparation"
 strCatagoryStreamNetworkSegmentation = "Main\\Step 2 - Stream Network Segmentation"
--- TransferAttributesToLine.py
+++ TransferAttributesToLine.py
@@ -1,15 +1,14 @@
 ﻿# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Name:        Transfer Linework Attributes Tool                              #
+# Name:        Transfer Attributes Tool                                       #
 # Purpose:     Transfer attributes from one line layer to another             #
 #                                                                             #
 # Author:      Kelly Whitehead (kelly@southforkresearch.org)                  #
-#              Jesse Langdon (jesse@southforkresearch.org                     #
+#              Jesse Langdon (jesse@southforkresearch.org)                    #
 #              South Fork Research, Inc                                       #
 #              Seattle, Washington                                            #
 #                                                                             #
 # Created:     2015-01-08                                                     #
-# Version:     2.0 (Beta)                                                     #
-# Modified:    2017-02-15                                                     #
+# Modified:    2017-05-03                                                     #
 #                                                                             #
 # Copyright:   (c) Kelly Whitehead, Jesse Langdon                             #
 #                                                                             #
@@ -18,66 +17,71 @@
 
 
 # Import modules
-import sys
 import arcpy
 import gis_tools
 import DividePolygonBySegment
 
+
 def main(fcFromLine,
          fcToLine,
          fcOutputLineNetwork,
-         tempWorkspace):
+         tempWorkspace=''):
 
     gis_tools.resetData(fcOutputLineNetwork)
 
-    if tempWorkspace == '':
-        tempWorkspace == "in_memory"
+    fcFromLineTemp = gis_tools.newGISDataset(tempWorkspace, "GNAT_TLA_FromLineTemp")
+    arcpy.MakeFeatureLayer_management(fcFromLine, "lyrFromLine")
+    arcpy.CopyFeatures_management("lyrFromLine", fcFromLineTemp)
 
-    # Make bounding polygon
+    # Make bounding polygon for "From" line feature class
+    arcpy.AddMessage("GNAT TLA: Create buffer polygon around 'From' network")
     fcFromLineBuffer = gis_tools.newGISDataset(tempWorkspace,"GNAT_TLA_FromLineBuffer")
-    arcpy.Buffer_analysis(fcFromLine,fcFromLineBuffer,"10 Meters","FULL","ROUND","ALL")
-    fcToLineBuffer = gis_tools.newGISDataset(tempWorkspace,"GNAT_TLA_ToLineBuffer")
-    arcpy.Buffer_analysis(fcToLine,fcToLineBuffer,"10 Meters","FULL","ROUND","ALL")
-    fcUnionBuffer = gis_tools.newGISDataset(tempWorkspace,"GNAT_TLA_UnionBuffer")
-    arcpy.Union_analysis([fcToLineBuffer,fcFromLineBuffer],fcUnionBuffer)
-    fcDissolveBuffer = gis_tools.newGISDataset(tempWorkspace,"GNAT_TLA_DissolveBuffer")
-    arcpy.Dissolve_management(fcUnionBuffer,fcDissolveBuffer)
-    fcFinalBuffer = gis_tools.newGISDataset(tempWorkspace,"GNAT_TLA_FinalBuffer")
-    arcpy.EliminatePolygonPart_management(fcDissolveBuffer,fcFinalBuffer,"PERCENT",part_area_percent="99.9",part_option="CONTAINED_ONLY")
+    arcpy.Buffer_analysis(fcFromLineTemp,fcFromLineBuffer,"10 Meters","FULL","ROUND","ALL")
 
+    # Select features from "To" line feature class that are inside "From" line buffer
+    lyrFromLineBuffer = gis_tools.newGISDataset("Layer", "lyrFromLineBuffer")
+    arcpy.MakeFeatureLayer_management(fcFromLineBuffer, lyrFromLineBuffer)
+    lyrToLine = gis_tools.newGISDataset("Layer", "lyrToLine")
+    arcpy.MakeFeatureLayer_management(fcToLine, lyrToLine)
+    arcpy.SelectLayerByLocation_management(lyrToLine, "WITHIN", lyrFromLineBuffer, "#", "NEW_SELECTION")
+    fcToLineWithinFromBuffer = arcpy.FeatureClassToFeatureClass_conversion(lyrToLine, tempWorkspace, "GNAT_TLA_ToLineWithinFromBuffer")
+
+    # Select features from "To" line feature class that are outside "From" line buffer
+    arcpy.SelectLayerByAttribute_management(lyrToLine, "SWITCH_SELECTION")
+    fcToLineOutsideFromBuffer = arcpy.FeatureClassToFeatureClass_conversion(lyrToLine, tempWorkspace, "GNAT_TLA_ToLineOutsideFromBuffer")
+
+    # Segment "From" line buffer polygon
     fcSegmentedBoundingPolygons = gis_tools.newGISDataset(tempWorkspace,"GNAT_TLA_SegmentedBoundingPolygons")
-    DividePolygonBySegment.main(fcFromLine,fcFinalBuffer,fcSegmentedBoundingPolygons)
+    DividePolygonBySegment.main(fcFromLineTemp, fcFromLineBuffer, fcSegmentedBoundingPolygons)
 
     # Split points of "To" line at intersection of polygon segments
     fcIntersectSplitPoints = gis_tools.newGISDataset(tempWorkspace,"GNAT_TLA_IntersectSplitPoints")
-    arcpy.Intersect_analysis([fcToLine,fcSegmentedBoundingPolygons],fcIntersectSplitPoints,output_type="POINT")
-
+    arcpy.Intersect_analysis([fcToLineWithinFromBuffer,fcSegmentedBoundingPolygons],fcIntersectSplitPoints,output_type="POINT")
     fcSplitLines = gis_tools.newGISDataset(tempWorkspace,"GNAT_TLA_SplitLines")
-    arcpy.SplitLineAtPoint_management(fcToLine,fcIntersectSplitPoints,fcSplitLines,"0.1 METERS")
+    arcpy.SplitLineAtPoint_management(fcToLineWithinFromBuffer,fcIntersectSplitPoints,fcSplitLines,"0.1 METERS")
 
-    # Spatial join lines based on a common FID, as transferred by segmented polygon
+    # Spatial join lines based on a common OID, as transferred by segmented polygon
+    arcpy.AddMessage("GNAT TLA: Joining polygon segments")
     gis_tools.resetData(fcOutputLineNetwork)
-
     arcpy.SpatialJoin_analysis(fcSplitLines,
                                fcSegmentedBoundingPolygons,
                                fcOutputLineNetwork,
                                "JOIN_ONE_TO_ONE",
                                "KEEP_ALL",
                                match_option="WITHIN")
+    arcpy.JoinField_management(fcOutputLineNetwork, "JOIN_FID", fcFromLineTemp, str(arcpy.Describe(fcFromLineTemp).OIDFieldName))
 
-    arcpy.JoinField_management(fcOutputLineNetwork, "JOIN_FID", fcFromLine, str(arcpy.Describe(fcFromLine).OIDFieldName))
-    
+    # Append the "To" lines that were outside of the "From" line buffer, which will have NULL or zero values
+    arcpy.env.extent = fcToLine # this was changed earlier in the process by  Divide Polygon by Segment script
+    arcpy.Append_management([fcToLineOutsideFromBuffer], fcOutputLineNetwork, "NO_TEST")
+    arcpy.AddMessage("GNAT TLA: Tool complete")
+
     return
 
-if __name__ == "__main__":
-    # fcToLine = r"C:\JL\Testing\GNAT\TransferLineAttributes\input.gdb\seg1000m_entiat"
-    # fcFromLine = r"C:\JL\Testing\GNAT\TransferLineAttributes\input.gdb\Entiat_EP_20160930"
-    # fcOutputLineNetwork = r"C:\JL\Testing\GNAT\TransferLineAttributes\output.gdb\test_Entiat"
-    # tempWorkspace = r"C:\JL\Testing\GNAT\TransferLineAttributes\scratch.gdb"
-    # main(fcFromLine, fcToLine, fcOutputLineNetwork, tempWorkspace)
 
-    main(sys.argv[1],
-         sys.argv[2],
-         sys.argv[3],
-         sys.argv[4],
-         sys.argv[5])
+# if __name__ == "__main__":
+#     fcToLine = r"C:\JL\Testing\GNAT\Issue29\data2\To.shp"
+#     fcFromLine = r"C:\JL\Testing\GNAT\Issue29\data2\From.shp"
+#     fcOutputLineNetwork = r"C:\JL\Testing\GNAT\Issue29\data2\Output.shp"
+#     tempWorkspace = r"C:\JL\Testing\GNAT\Issue29\data2\scratch.gdb"
+#     main(fcFromLine, fcToLine, fcOutputLineNetwork, tempWorkspace)
