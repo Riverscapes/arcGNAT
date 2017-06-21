808959c4ee369179dfc0c70b68591e1b1b0b5750
jesselangdon <jesselangdon@gmail.com>
2017-06-21 10:17:01 -0700

    Reduced search buffer distances to 0.001.

--- BuildNetworkTopology.py
+++ BuildNetworkTopology.py
@@ -74,9 +74,9 @@ def network_tree(inputID,tblNetwork,fcLines,fcNodePoint):
         if inputID in listBraidedReaches:
             arcpy.SelectLayerByAttribute_management("lyrBraidedReachStartPoints","CLEAR_SELECTION")
             arcpy.MakeFeatureLayer_management("lyrBraidedReachStartPoints","lyrCurrentReachBraidedPoint",""" "ORIG_FID" = """ + str(inputID))
-            arcpy.SelectLayerByLocation_management("SelectedReaches","WITHIN_A_DISTANCE","lyrCurrentReachBraidedPoint","0.1","NEW_SELECTION")
+            arcpy.SelectLayerByLocation_management("SelectedReaches","WITHIN_A_DISTANCE","lyrCurrentReachBraidedPoint","0.001","NEW_SELECTION")
 
-            arcpy.SelectLayerByLocation_management("lyrBraidedReachStartPoints","WITHIN_A_DISTANCE","lyrCurrentReachBraidedPoint","0.1","NEW_SELECTION")
+            arcpy.SelectLayerByLocation_management("lyrBraidedReachStartPoints","WITHIN_A_DISTANCE","lyrCurrentReachBraidedPoint","0.001","NEW_SELECTION")
             arcpy.AddMessage("Braided " + str(int(arcpy.GetCount_management("lyrBraidedReachStartPoints").getOutput(0))))
             arcpy.SelectLayerByAttribute_management("lyrBraidedReachStartPoints","REMOVE_FROM_SELECTION",""" "ORIG_FID" = """ + str(inputID))
             arcpy.SelectLayerByAttribute_management("SelectedReaches","REMOVE_FROM_SELECTION",oid_fcLines + " = " + str(inputID))
@@ -89,9 +89,9 @@ def network_tree(inputID,tblNetwork,fcLines,fcNodePoint):
             listSelected = descSelectedReaches.FIDset.split("; ")
 
         else:
-            arcpy.SelectLayerByLocation_management("SelectedReaches","WITHIN_A_DISTANCE","InputReach","0.1","NEW_SELECTION")
+            arcpy.SelectLayerByLocation_management("SelectedReaches","WITHIN_A_DISTANCE","InputReach","0.001","NEW_SELECTION")
             arcpy.SelectLayerByAttribute_management("SelectedReaches","REMOVE_FROM_SELECTION",oid_fcLines + " = " + str(inputID))
-            arcpy.SelectLayerByLocation_management("lyrBraidedReachStartPoints","WITHIN_A_DISTANCE","InputReach","0.1","NEW_SELECTION")
+            arcpy.SelectLayerByLocation_management("lyrBraidedReachStartPoints","WITHIN_A_DISTANCE","InputReach","0.001","NEW_SELECTION")
             listSelectedBraidedReaches = []
             if int(arcpy.GetCount_management("lyrBraidedReachStartPoints").getOutput(0)) > 0:
                 with arcpy.da.SearchCursor("lyrBraidedReachStartPoints",["ORIG_FID"]) as scBraided:
